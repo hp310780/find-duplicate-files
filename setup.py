d4905482065717576c970c4fe153987fa74a9ac2
hp310780 <43389959+hp310780@users.noreply.github.com>
2019-04-21 11:58:23 +0100

    pip package requirements

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+     name='FindDuplicateFiles',
+     version='0.1',
+     scripts=['find_duplicate_files.py'],
+     author="hp310780",
+     description="Module to find duplicate files in a directory",
+     url="https://github.com/hp310780/FindDuplicateFiles",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3"
+     ],
+
+ )
